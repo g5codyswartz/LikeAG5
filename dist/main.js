fd5365066ed78a0bcaeb3aae900d76685d9c975c
Cody Swartz <cody.swartz@getg5.com>
2017-01-19 18:34:34 -0800

    Following TS Gulp Page https://www.typescriptlang.org/docs/handbook/gulp.html

--- dist/main.js
+++ dist/main.js
@@ -4,33 +4,23 @@
 /// <reference path="../typescript/NamedWidgets.ts" />
 /// <reference path="../plugins/EditRow.ts" />
 /// <reference path="../libraries/definitions/chrome.d.ts" />
-
 //import AutoPlaceholer = require("typescript/AutoPlaceholder");
-
-$( ()=> {
-
+$(function () {
     console.log("LikeAG5 Initialized");
-
     //var hooks = new LikeAG5.Hooks();
     //var autoPlaceholder = new LikeAG5.AutoPlaceholder;
-	//var editRow = new LikeAG5.EditRow;
-
-	// crashes chrome currently
+    //var editRow = new LikeAG5.EditRow;
+    // crashes chrome currently
     //autoPlaceholder.inject(hooks);
-	//editRow.inject(hooks);
-
-	/*var port = chrome.runtime.connect({name: "LikeAG5"});
-	port.postMessage({joke: "Knock knock"});
-	port.onMessage.addListener(function(msg:any) {
-		console.log("main msg");
-		console.log(msg);
-		if (msg.question == "Who's there?")
-			port.postMessage({answer: "Madame"});
-		else if (msg.question == "Madame who?")
-			port.postMessage({answer: "Madame... Bovary"});
-	});*/
-
+    //editRow.inject(hooks);
+    /*var port = chrome.runtime.connect({name: "LikeAG5"});
+    port.postMessage({joke: "Knock knock"});
+    port.onMessage.addListener(function(msg:any) {
+        console.log("main msg");
+        console.log(msg);
+        if (msg.question == "Who's there?")
+            port.postMessage({answer: "Madame"});
+        else if (msg.question == "Madame who?")
+            port.postMessage({answer: "Madame... Bovary"});
+    });*/
 });
-
-
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task('default', function () {
-	return gulp.src('js/**/*.ts')
-	.pipe(ts({
-		noImplicitAny: true,
-		out: 'output.js'
-	}))
-	.pipe(gulp.dest('built/local'));
+gulp.task("default", function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("dist"));
 });
